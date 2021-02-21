8036731465dd2eca60ffb93613dc85d664996c30
Faraz Abidi <farazabidi@Farazs-MacBook-Air-3.local>
2021-02-20 22:02:39 -0800

    Finished SalesListings.

--- TopshotQueries/Components/SalesListingAnalyzer/SalesListingAnalyzer.js
+++ TopshotQueries/Components/SalesListingAnalyzer/SalesListingAnalyzer.js
@@ -1,19 +1,60 @@
-module.exports = class SalesListingAnalyzer{
-    
+const DataQueries = require('../DataQueries/DataQueries')
+
+module.exports = class SalesListingAnalyzer{   
     
     /**
      * Checks all sales listings within each moment to see if there is one that is SIGNIFICANTLY
      * lower than all of the other ones in price. 
      * 
      * We define significant as 50% or lower.
-     * //TODO: Change the threshold
+     * //TODO: Make the threshold a parameter
      */
     getLowOutliersForAllMoments(){
-        //TODO
-        console.log("SalesListingAnalyzer::getLowOutliersForAllMoments");
+        const outlierPercentThreshold = .5;
+
+        const allMomentListings =  DataQueries.getAllActiveSalesListingsGroupedByMoment();
+
+        let outlierMomentListings = [];
+        for(singleMomentListings in allMomentListings){
+            singleMomentListings = this.analyzeMomentListingsForOutliers(singleMomentListings);
+            if(singleMomentListings.percentFromCheapestToSecondCheapest < outlierPercentThreshold)
+                outlierMomentListings.push(singleMomentListings);
+        }
+
+        return outlierMomentListings;
     }
 
     getHistoryOfCheapestListingForMoment(momentID){
         console.log("SalesListingAnalyzer::getHistoryOfCheapestListingForMoment: " + momentID);
+        //TODO
+    }
+
+    analyzeMomentListingsForOutliers(momentListings){
+        let cheapestListing, secondCheapestListing;
+
+        for(listing in momentListings.listings){
+            if(cheapestListing === null)
+                cheapestListing = listing;
+            else if(secondCheapestListing === null)
+                secondCheapestListing = listing;
+            else{
+                if(listing.price < cheapestListing.price){
+                    secondCheapestListing = cheapestListing;
+                    cheapestListing = listing;
+                }
+                else if(listing.price < secondCheapestListing.price){
+                    secondCheapestListing = listing;
+                }
+            }
+        }
+
+        if(momentListings.listings.length() < 10)
+            momentListings.percentFromCheapestToSecondCheapest = 100000;            
+        else{
+            momentListings.percentFromCheapestToSecondCheapest = (cheapestListing.price.toFixed(2))/(secondCheapestListing.price.toFixed(2));
+        }
+
+        momentListings.cheapestListing = cheapestListing;
+        return momentListings;
     }
 }
