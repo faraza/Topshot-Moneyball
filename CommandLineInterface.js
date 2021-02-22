0a900680384a9f02cf61b48f8cad0ad73f9723c6
Faraz Abidi <farazabidi@farazs-air-3.lan>
2021-02-22 11:06:49 -0800

    Added Website scraper.

--- CommandLineInterface.js
+++ CommandLineInterface.js
@@ -104,5 +104,21 @@ async function printListings(){
 
 
 
-let test = new TopshotQueries();
-console.log(test.getLowOutlierSalesListingsOfAllMoments());
+// let test = new TopshotQueries();
+// console.log(test.getLowOutlierSalesListingsOfAllMoments());
+
+
+//https://cryptoslam.io/nba-top-shot/run/?Name=Jaylen%20Brown
+function scrapeWebsite(){
+    const endpointStart = 'https://cryptoslam.io/nba-top-shot/run/?Name=';
+    let playerNames; //TODO: get it from the file Bryan got
+
+    for(player in playerNames){
+        player = player.trim();
+        player.replace(' ', '%20');
+        const endpoint = endpointStart + player;
+
+        //TODO: Scrape it
+    }
+
+}
